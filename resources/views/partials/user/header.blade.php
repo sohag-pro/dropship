517a9d6c0d86e239bd339e9d24171ae537038e7c
shuvo575 <shohaghasan575@gmail.com>
2019-11-07 23:46:48 +0600

    home page auth added

--- resources/views/partials/user/header.blade.php
+++ resources/views/partials/user/header.blade.php
@@ -1,10 +1,12 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
 <head>
   <meta charset="utf-8" />
-  <link rel="apple-touch-icon" sizes="76x76" href="./assets/img/apple-icon.png">
-  <link rel="icon" type="image/png" href="./assets/img/favicon.png">
+  <!-- CSRF Token -->
+  <meta name="csrf-token" content="{{ csrf_token() }}">
+  {{-- <link rel="apple-touch-icon" sizes="76x76" href="./assets/img/apple-icon.png">
+  <link rel="icon" type="image/png" href="./assets/img/favicon.png"> --}}
   <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
   <title>
     SendCargo - @yield('title')
--- resources/views/partials/user/nav.blade.php
+++ resources/views/partials/user/nav.blade.php
@@ -1,7 +1,7 @@
 <nav class="navbar navbar-transparent navbar-color-on-scroll fixed-top navbar-expand-lg" color-on-scroll="100" id="sectionsNav">
     <div class="container">
       <div class="navbar-translate">
-        <a class="navbar-brand" href="https://demos.creative-tim.com/material-kit/index.html">
+        <a class="navbar-brand" href="{{ url('/') }}">
           SendCargo </a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" aria-expanded="false" aria-label="Toggle navigation">
           <span class="sr-only">Toggle navigation</span>
@@ -13,25 +13,51 @@
       <div class="collapse navbar-collapse">
         <ul class="navbar-nav ml-auto">
           <li class="dropdown nav-item">
-            <a href="{{ route('index') }}" class="nav-link">
+            <a href="{{ route('home') }}" class="nav-link">
               <i class="material-icons">house</i> Home
             </a>
           </li>
-          <li class="dropdown nav-item">
+          <li class="nav-item">
             <a href="{{ route('order.create') }}" class="nav-link">
               <i class="material-icons">add_shopping_cart</i> New Order
             </a>
           </li>
-          <li class="dropdown nav-item">
-            <a href="order.html" class="nav-link">
-              <i class="material-icons">shopping_basket</i> My Orders
-            </a>
-          </li>
-          <li class="dropdown nav-item">
-            <a href="profile.html" class="nav-link">
-              <i class="material-icons">person</i> My Profile
-            </a>
-          </li>
+          @guest
+            <li class="nav-item">
+              <a href="{{ route('login') }}" class="nav-link">
+                <i class="material-icons">fingerprint</i> {{ __('Login') }}
+              </a>
+            </li>
+            @if (Route::has('register'))
+                <li class="nav-item">
+                  <a href="{{ route('register') }}" class="nav-link">
+                    <i class="material-icons">person_add</i> {{ __('Register') }}
+                  </a>
+                </li>
+            @endif
+          @else
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    {{ Auth::user()->name }}
+                </a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                    <a href="profile.html" class="nav-link">
+                      <i class="material-icons">person</i> My Profile
+                    </a>
+                    <a href="order.html" class="nav-link">
+                      <i class="material-icons">shopping_basket</i> My Orders
+                    </a>
+                  <a class="nav-link" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                      <i class="material-icons">directions_run</i>{{ __('Logout') }}
+                  </a>
+                  <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                      @csrf
+                  </form>
+                  {{-- <a class="dropdown-item" href="#">Another action</a>
+                  <a class="dropdown-item" href="#">Something else here</a> --}}
+                </div>
+            </li>
+          @endguest
         </ul>
       </div>
     </div>
